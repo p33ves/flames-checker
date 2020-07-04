d8af7d331e1035b19324e9cd6b5461925b10369a
Vignesh P <vignesh.p1110@gmail.com>
2020-07-04 21:57:08 +0530

    get input names

--- flames_checker.py
+++ flames_checker.py
@@ -1,3 +1,16 @@
+import string
+
+
+def get_name(name: str) -> list:
+    temp = list(name.lower())
+    for char in temp:
+        if char not in set(string.ascii_lowercase):
+            temp.remove(char)
+    return temp
+
 
 if __name__ == "__main__":
-    pass
+    name1 = input("Enter first person's name:")
+    first = get_name(name1)
+    name2 = input("Enter second person's name:")
+    second = get_name(name2)
